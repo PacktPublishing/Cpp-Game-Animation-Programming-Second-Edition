6cc8b377d154482ff469fc9edb40096ecf7b48a4
Michael Dunsky <michaeldy@mailbox.org>
2023-05-14 00:07:42 +0200

    remove renamed examples from chapter 9

--- chapter09/04_vulkan_gltf_bindpose/tools/Logger.cpp
+++
@@ -1,3 +0,0 @@
-#include "Logger.h"
-
-unsigned int Logger::mLogLevel = 1;
--- chapter09/04_vulkan_gltf_bindpose/tools/Tools.cpp
+++
@@ -1,9 +0,0 @@
-#include "Tools.h"
-
-std::string Tools::getFilenameExt(std::string filename) {
-  size_t pos = filename.find_last_of('.');
-  if (pos != std::string::npos) {
-    return filename.substr(pos + 1);
-  }
-  return std::string();
-}
