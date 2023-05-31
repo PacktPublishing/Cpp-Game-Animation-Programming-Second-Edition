cc2f51df66402b99b8f791d2b2a877a351b713d3
Michael Dunsky <michaeldy@mailbox.org>
2023-05-31 12:04:27 +0200

    animation code for chapter 10

---
+++ chapter10/01_opengl_animations/model/GltfAnimationClip.cpp
@@ -0,0 +1,34 @@
+#include "GltfAnimationClip.h"
+
+GltfAnimationClip::GltfAnimationClip(std::string name) : mClipName(name) {
+}
+
+void GltfAnimationClip::addChannel(std::shared_ptr<tinygltf::Model> model, tinygltf::Animation anim, tinygltf::AnimationChannel channel) {
+  std::shared_ptr<GltfAnimationChannel> chan = std::make_shared<GltfAnimationChannel>();
+  chan->loadChannelData(model, anim, channel);
+  mAnimationChannels.push_back(chan);
+}
+
+void GltfAnimationClip::setAnimationFrame(std::vector<std::shared_ptr<GltfNode>> nodes, float time) {
+  for (auto &channel : mAnimationChannels) {
+    int targetNode = channel->getTargetNode();
+    if (channel->getTargetPath() == ETargetPath::ROTATION) {
+      nodes.at(targetNode)->setRotation(channel->getRotation(time));
+    }
+    if (channel->getTargetPath() == ETargetPath::TRANSLATION) {
+      nodes.at(targetNode)->setTranslation(channel->getTranslation(time));
+    }
+    if (channel->getTargetPath() == ETargetPath::SCALE) {
+      nodes.at(targetNode)->setScale(channel->getScaling(time));
+    }
+    nodes.at(targetNode)->calculateLocalTRSMatrix();
+  }
+}
+
+float GltfAnimationClip::getClipEndTime() {
+  return mAnimationChannels.at(0)->getMaxTime();;
+}
+
+std::string GltfAnimationClip::getClipName() {
+  return mClipName;
+}
